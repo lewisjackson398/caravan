71c474e61c87daf733f75baae8ed7f05c5a08e53
lewisjackson398 <lewisjackson398@gmail.com>
2020-08-13 22:14:57 +0100

    Updates

---
+++ precache-manifest.d7ad2d391be99ec9ca03648ddffdb640.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a760afb8860c1ea1b4b475c27544cf3",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "0ac8438ed76741665256",
+    "url": "/caravan/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "976ee7175020ac993fc7",
+    "url": "/caravan/static/js/2.0462a28c.chunk.js"
+  },
+  {
+    "revision": "e1690def25f3a7d9aa00a43cf9fa9296",
+    "url": "/caravan/static/js/2.0462a28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac8438ed76741665256",
+    "url": "/caravan/static/js/main.698d3cea.chunk.js"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/caravan/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "4074f3302350deea73576874d05fb147",
+    "url": "/caravan/static/media/background.4074f330.jpeg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/caravan/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/caravan/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/caravan/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/caravan/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/caravan/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "4135889541361c5c5fa7cde8a608c3c0",
+    "url": "/caravan/static/media/caravan1.41358895.jpeg"
+  },
+  {
+    "revision": "67f91f2e83eeb302e7458649efb346c3",
+    "url": "/caravan/static/media/caravan2.67f91f2e.jpeg"
+  },
+  {
+    "revision": "5fe23a287968e9f0f0d1233035fe5c20",
+    "url": "/caravan/static/media/caravan3.5fe23a28.jpeg"
+  },
+  {
+    "revision": "c762a61cc39f81a4329407a5637c3941",
+    "url": "/caravan/static/media/caravan4.c762a61c.jpeg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/caravan/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/caravan/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/caravan/static/media/sign.cf8b686b.jpg"
+  }
+]);
